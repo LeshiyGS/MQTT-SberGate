81eda9e374eb5ddce322f27a51d4a98c8fee5664
LeshiyGS <k.leshemy@gmail.com>
2023-08-01 13:18:29 +0300

    fix

--- addons/mqtt-sber-gate/rootfs/app/sber-gate.py
+++ addons/mqtt-sber-gate/rootfs/app/sber-gate.py
@@ -406,7 +406,7 @@ log('SberDevice http_api_endpoint: '+Options['sber-http_api_endpoint'])
 hds = {'content-type': 'application/json'}
 if not os.path.exists('models.json'):
    log('Файл моделей отсутствует. Получаем...')
-   SD_Models = requests.get(Options['sber-http_api_endpoint']+'/v1/mqtt-gate/models', headers=hds,auth=(Options['sber-mqtt_login'], Options['sber-mqtt_password']))
+   SD_Models = requests.get(Options['sber-http_api_endpoint']+'/v1/mqtt-gate/models', headers=hds, auth=(Options['sber-mqtt_login'], Options['sber-mqtt_password']))
    with open('models.json', 'w') as f:
     json.dump(SD_Models.json(), f)
    #SD_Models = requests.get(Options['sber-http_api_endpoint']+'/v1/mqtt-gate/models', headers=hds,auth=(Options['sber-mqtt_login'], Options['sber-mqtt_password'])).json()
