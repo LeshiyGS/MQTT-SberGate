0d26ae671738b7c837a5a947a3c9c6c46ea89bc6
LeshiyGS <k.leshemy@gmail.com>
2023-08-02 15:04:00 +0300

    fix

--- addons/mqtt-sber-gate/rootfs/app/sber-gate.py
+++ addons/mqtt-sber-gate/rootfs/app/sber-gate.py
@@ -75,13 +75,13 @@ def ha_upd_switch():
    while True:
       sleep(10)
       for k in DevicesDB.DB:
-        #log(DevicesDB.DB[k]['enabled'])
         if DevicesDB.DB[k]['enabled'] == True:
+          log(k)
           #hds = {'Authorization': 'Bearer '+Options['ha-api_token'], 'content-type': 'application/json'}
           #url=Options['ha-api_url']+'/api/states/'+k
           #res = requests.get(url, headers=hds)
           #log(res.json())
-          log(k)
+          
           #log(DevicesDB.DB[k]['enabled'])
      
 
