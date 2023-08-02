454ae5a2ce719fc9ae7e08184b3edd22f5020c3c
LeshiyGS <k.leshemy@gmail.com>
2023-08-02 15:54:08 +0300

    fix

--- addons/mqtt-sber-gate/rootfs/app/sber-gate.py
+++ addons/mqtt-sber-gate/rootfs/app/sber-gate.py
@@ -80,7 +80,7 @@ def ha_upd_switch():
             url=Options['ha-api_url']+'/api/states/'+k
             res = requests.get(url, headers=hds)
             temp = res.json()
-            log(temp['state'])
+            #log(temp['state'])
       sleep(10)
       
 
