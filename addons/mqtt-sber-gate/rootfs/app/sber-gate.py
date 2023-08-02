54cfb2c48aab7be9bb68a3c9c4ab5af62c3766cd
LeshiyGS <k.leshemy@gmail.com>
2023-08-02 12:18:14 +0300

    test

--- addons/mqtt-sber-gate/rootfs/app/sber-gate.py
+++ addons/mqtt-sber-gate/rootfs/app/sber-gate.py
@@ -352,7 +352,7 @@ log(Options['ha-api_url'])
 hds = {'Authorization': 'Bearer '+Options['ha-api_token'], 'content-type': 'application/json'}
 url=Options['ha-api_url']+'/api/states'
 res = requests.get(url, headers=hds).json()
-log(res)
+#log(res)
 
 
 def upd_sw(id,s):
@@ -368,9 +368,7 @@ def upd_light(id,s):
    log('light: ' + s['entity_id'] + ' '+attr)
    DevicesDB.update(s['entity_id'],{'entity_ha': True,'entity_type': 'light','friendly_name':attr,'category': 'light'})
 def upd_default(id,s):
-   qq=id
-   #log('')
-   #log('Неиспользуемый тип: ' + s['entity_id'])
+   log('Неиспользуемый тип: ' + s['entity_id'])
 for s in res:
    a,b=s['entity_id'].split('.',1)
    dict={
